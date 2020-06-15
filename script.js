1268c100483e233fc60f774b7ed42f7a415bae1c
ATTHAKORN PITJAYANONT <38579028+manenigma@users.noreply.github.com>
2020-06-15 13:35:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var li = document.querySelectorAll("li");
+
+
+function inputLength() {
+	return input.value.length;
+}
+
+function creteListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		creteListElement();
+	}
+}
+
+function addListAfterEnter(event) {
+	if (inputLength() > 0 && event.which === 13) {
+		creteListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick)
+
+input.addEventListener("keypress", addListAfterEnter)
+
+ul.addEventListener('click', function(e) {
+	if (e.target.nodeName === "SPAN") {
+		e.target.classList.toggle("done");
+	} else if (e.target.nodeName === "BUTTON") {
+		e.target.parentElement.remove();
+	}
+});
